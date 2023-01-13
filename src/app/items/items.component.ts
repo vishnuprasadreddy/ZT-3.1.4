3afa9a822521cd909597e6bbde7c8de1cec5935e
vishnu <vishnuprasadreddy444@gmail.com>
2023-01-13 13:23:09 +0530

    changes

--- src/app/items/items.component.ts
+++ src/app/items/items.component.ts
@@ -1,6 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
-import { ItemsModal } from './modal/modal.items';
 
 
 
--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Output,EventEmitter} from '@angular/core';
-import { SidebarModel } from './model/sidebar.model';
+
 
 @Component({
   selector: 'app-sidebar',
@@ -9,25 +9,12 @@ import { SidebarModel } from './model/sidebar.model';
 export class SidebarComponent implements OnInit {
   
  
-  @Output() ParentData:EventEmitter<any> = new EventEmitter(); 
+  
   constructor() { }
 
   ngOnInit(): void {  
   }
 
-  sidebarItemClick(sidebarData:string){
-    this.ParentData.emit(sidebarData);  
-  }
 
-
-
-  sidebarArray  :SidebarModel[] = [
-    new SidebarModel('Items','bi bi-journals'),
-    new SidebarModel('Tests','bi bi-file-earmark-font'),
-    new SidebarModel('Test Takers','bi bi-person-fill'),
-    new SidebarModel('Groups','bi bi-people-fill'),
-    new SidebarModel('Deliveries','bi bi-file-text-fill'),
-    new SidebarModel('Results','fas fa-graduation-cap'),
-  ]
 
 }
